11cdf2e27a75af0d055b19da01c13b5f9b79b45b
Davlatshoh <davlatshoh6275@gmail.com>
2023-10-03 19:36:06 +0300

    created about section

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -5,69 +5,94 @@ import { BsTelephoneFill } from "react-icons/bs";
 import { TbWorld } from "react-icons/tb";
 
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-	return (
-		<>
-			<header className="w-full bg-white">
-				<div className="flex justify-between items-center py-4 w-full max-w-[1920px] my-0 mx-auto px-10">
-					<div className="flex items-center gap-10">
-						<Image
-							src={"/icons/logo.svg"}
-							alt=""
-							width="80"
-							height="80"
-						/>
-						<nav className="flex items-center gap-6">
-							<Link href="#" className="font-semibold">
-								Home
-							</Link>
-							<Link href="#" className="font-semibold">
-								Home
-							</Link>
-							<Link href="#" className="font-semibold">
-								Home
-							</Link>
-							<Link href="#" className="font-semibold">
-								Home
-							</Link>
-						</nav>
-					</div>
-					<div className="flex items-center gap-5">
-						<button className="flex items-center gap-2 py-2 px-5 font-semibold">
-							RU <TbWorld size="25" />
-						</button>
-						<button className="flex items-center gap-2 py-2 px-5 text-white font-semibold bg-main rounded-full">
-							Перезвоните мне
-							<BsTelephoneFill />
-						</button>
-					</div>
-				</div>
-			</header>
-			<main>{children}</main>
-			<footer className="w-full bg-white" >
-                <div className="flex items-start justify-between max-w-[1040px] w-full mx-auto" >
-                    <nav className="flex flex-col items-start gap-3 " >
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                    </nav>
-                    <nav className="flex flex-col items-start gap-3 " >
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                        <Link href="#" className="font-semibold" >Главная</Link>
-                    </nav>
-                    <div className="flex flex-col items-start" >
-                        <Image src="/icons/black_logo.svg" alt="" width="80" height="44" className="mb-8" />
-                        <Link href="" className="font-semibold text-[44px]" >+998 (99) 911-01-11</Link>
-                        <span className="font-semibold text-[22px] text-[#A3A7AC] " >Буюк Ипак Йули 52, Самарканд, Узбекистан</span>
-                    </div>
-                </div>
-            </footer>
-		</>
-	);
+  return (
+    <>
+      <header className="w-full bg-white">
+        <div className="flex justify-between items-center py-4 w-full max-w-[1920px] my-0 mx-auto px-10">
+          <div className="flex items-center gap-10">
+            <Image src={"/icons/logo.svg"} alt="" width="80" height="80" />
+            <nav className="flex items-center gap-6">
+              <Link href="#" className="font-semibold">
+                Home
+              </Link>
+              <Link href="#" className="font-semibold">
+                Home
+              </Link>
+              <Link href="#" className="font-semibold">
+                Home
+              </Link>
+              <Link href="#" className="font-semibold">
+                Home
+              </Link>
+            </nav>
+          </div>
+          <div className="flex items-center gap-5">
+            <button className="flex items-center gap-2 py-2 px-5 font-semibold">
+              RU <TbWorld size="25" />
+            </button>
+            <button className="flex items-center gap-2 py-2 px-5 text-white font-semibold bg-main rounded-full">
+              Перезвоните мне
+              <BsTelephoneFill />
+            </button>
+          </div>
+        </div>
+      </header>
+      <main>{children}</main>
+      <footer className="w-full bg-white">
+        <div className="flex items-start justify-between max-w-[1040px] w-full mx-auto">
+          <nav className="flex flex-col items-start gap-3 ">
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+          </nav>
+          <nav className="flex flex-col items-start gap-3 ">
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+            <Link href="#" className="font-semibold">
+              Главная
+            </Link>
+          </nav>
+          <div className="flex flex-col items-start">
+            <Image
+              src="/icons/black_logo.svg"
+              alt=""
+              width="80"
+              height="44"
+              className="mb-8"
+            />
+            <Link href="" className="font-semibold text-[44px]">
+              +998 (99) 911-01-11
+            </Link>
+            <span className="font-semibold text-[22px] text-[#A3A7AC] ">
+              Буюк Ипак Йули 52, Самарканд, Узбекистан
+            </span>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Layout;
---
+++ pages/about/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Layout from "@/layouts/Layout";
+import OurGoal from "@/components/OurGoal";
+
+// md:700
+// sm: 600
+// lg: 1024
+// xl: 1280
+
+export default function AboutUs() {
+  return (
+    <Layout>
+      <section className=" pt-5 max-w-[1650px] mx-auto mb-32  ">
+        <div className="w-full px-9 sm:px-9 xl:ml-10 xl:w-3/4 f-full  ">
+          <p className="text-xl font-medium sm:text-4xl  sm:leading-snug mb-3  xl:text-5xl xl:font-semibold text-black xl:leading-tight xl:mb-10 ">
+            <span className="text-[#068FFF] font-extrabold ">Наша цель</span> -
+            помочь вам достичь онлайн-успеха, предоставляя инновационные решения
+            и высокий уровень профессионализма.
+          </p>
+          <p className=" text-lg font-normal  sm:text-2xl  xl:text-4xl  leading-normal ">
+            DOT LABS - Ваш надежный партнер в веб-разработке
+          </p>
+        </div>
+        <section className="grid grid-cols-3 gap-3 w-full max-w-[1650px] mx-auto border " >
+          <OurGoal/>
+          <OurGoal/>
+          <OurGoal/>
+        </section>
+      </section>
+    </Layout>
+  );
+}

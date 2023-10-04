72ff6edb617f87fbd8281ca33501a126a70a60ee
Daler <dsarifkulo021@gmail.com>
2023-10-04 18:00:40 +0500

    footer responsibility is ready too

--- layouts/Layout.tsx
+++ layouts/Layout.tsx
@@ -41,56 +41,58 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 							Перезвоните мне
 							<BsTelephoneFill />
 						</button>
-						<button className="md:hidden" >
+						<button className="md:hidden">
 							<GoGlobe size="26" />
 						</button>
-						<button className="md:hidden" >
+						<button className="md:hidden">
 							<BsFillTelephoneFill size="26" />
 						</button>
-						<button className="md:hidden" >
+						<button className="md:hidden">
 							<SlMenu size="26" />
 						</button>
 					</div>
 				</div>
 			</header>
-			<main>{children}</main>
-			<footer className="w-full bg-white">
-				<div className="flex items-start justify-between max-w-[1040px] w-full mx-auto">
-					<nav className="flex flex-col items-start gap-3 ">
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-					</nav>
-					<nav className="flex flex-col items-start gap-3 ">
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-						<Link href="#" className="font-semibold">
-							Главная
-						</Link>
-					</nav>
-					<div className="flex flex-col items-start">
+			{/* <main>{children}</main> */}
+			<footer className="w-full bg-white my-10 px-6">
+				<div className="flex items-start justify-between flex-col-reverse gap-4 md:flex-row max-w-[1040px] w-full mx-auto">
+					<div className="flex items-start justify-between w-full md:w-[50%]">
+						<nav className="flex flex-col items-start gap-3 ">
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+						</nav>
+						<nav className="flex flex-col items-start gap-3">
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+							<Link href="#" className="font-semibold">
+								Главная
+							</Link>
+						</nav>
+					</div>
+					<div className="flex flex-col items-start mb-5">
 						<Image
 							src="/icons/black_logo.svg"
 							alt=""
@@ -98,14 +100,17 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 							height="44"
 							className="mb-8"
 						/>
-						<Link href="" className="font-semibold text-[44px]">
+						<Link href="" className="font-semibold text-[32px] md:text-[44px]">
 							+998 (99) 911-01-11
 						</Link>
-						<span className="font-semibold text-[22px] text-[#A3A7AC] ">
+						<span className="font-semibold text-[14px] md:text-[22px] text-[#A3A7AC] ">
 							Буюк Ипак Йули 52, Самарканд, Узбекистан
 						</span>
 					</div>
 				</div>
+				<span className="block max-w-[1040px] w-full mx-auto mt-10 text-[#A3A7AC] ">
+					© Dot Labs 2023. Все права защищены
+				</span>
 			</footer>
 		</>
 	);

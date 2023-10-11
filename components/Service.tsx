017ad08b7bd5cbd2b4a7c0e15f6766c3821734a7
Daler <dsarifkulo021@gmail.com>
2023-10-11 12:52:11 +0500

    fixed service card

--- components/Service.tsx
+++ components/Service.tsx
@@ -10,31 +10,31 @@ const Service: React.FC<ServiceProps> = () => {
 		<div
 			className="w-full bg-white rounded-[24px] overflow-hidden custom-shadow"
 		>
-			<div className="flex flex-col items-start p-7">
+			<div className="flex flex-col items-start p-5">
 				<h2 className="text-3xl font-bold mb-3">Веб-сайты</h2>
-				<p className="font-normal">
+				<p className="font-normal text-[15px] md:text-[16px] ">
 					Наши опытные дизайнеры и разработчики создадут для вас
 					современный, функциональный и креативный веб-сайт, который
 					выделяется среди конкурентов.
 				</p>
 				<div className="flex flex-col items-start gap-4 mt-4">
 					<div className="flex items-center gap-3">
-						<div className="flex items-center justify-center w-[32px] h-[32px] bg-[#E9F0F9] rounded-full">
+						<div className="flex items-center justify-center min-w-[32px] min-h-[32px] bg-[#E9F0F9] rounded-full">
 							<SiFigma color="#068FFF" size="20" />
 						</div>
-						<span className="font-semibold" >Уникальный дизайн, отражающий ваш бренд</span>
+						<span className="font-normal md:font-semibold text-sm " >Уникальный дизайн, отражающий ваш бренд</span>
 					</div>
 					<div className="flex items-center gap-3">
-						<div className="flex items-center justify-center w-[32px] h-[32px] bg-[#E9F0F9] rounded-full">
+						<div className="flex items-center justify-center min-w-[32px] min-h-[32px] bg-[#E9F0F9] rounded-full">
 							<AiOutlineMobile color="#068FFF" size="20" />
 						</div>
-						<span className="font-semibold" >Уникальный дизайн, отражающий ваш бренд</span>
+						<span className="font-normal md:font-semibold text-sm " >Уникальный дизайн, отражающий ваш бренд</span>
 					</div>
 					<div className="flex items-center gap-3">
-						<div className="flex items-center justify-center w-[32px] h-[32px] bg-[#E9F0F9] rounded-full">
+						<div className="flex items-center justify-center min-w-[32px] min-h-[32px] bg-[#E9F0F9] rounded-full">
 							<AiOutlineLike color="#068FFF" size="20" />
 						</div>
-						<span className="font-semibold" >Уникальный дизайн, отражающий ваш бренд</span>
+						<span className="font-normal md:font-semibold text-sm " >Уникальный дизайн, отражающий ваш бренд</span>
 					</div>
 				</div>
 			</div>

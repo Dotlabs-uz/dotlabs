d9a3e9b9b7db9d518bbb8cee03c25fa4b9d738b6
Daler <dsarifkulo021@gmail.com>
2023-10-23 18:05:53 +0500

    orders is ready

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,18 +1,58 @@
 import React, { useState } from "react";
 import aboutCss from "../styles/about.module.scss";
+import { useForm, SubmitHandler } from "react-hook-form";
+import axios from "axios";
+
 
 export default function Contact() {
+	type userData = { userName: string; phone: string };
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<userData>();
+
+	const onSubmit: SubmitHandler<userData> = async (data) => {
+		try {
+			const res = await axios.post('https://sheet.best/api/sheets/f24ba5dc-114e-4649-9fcd-68b5ca52992f', data)
+
+			if(res.status === 200 || res.status === 201) {
+				const body = {
+					chat_id: '-1002062552409',
+					parse_mode: "html",
+					text: `
+						Новая заявка 
+						<a href="https://docs.google.com/spreadsheets/d/1Anq1vHx9tCmmDQAMa7A-MQ41S8culwb1fMIm7AYC7Dc/edit#gid=0" >
+							Взгянуть на таблицу
+						</a>
+					`,
+	
+				}
+				axios.post(`https://api.telegram.org/bot${'6710636505:AAHDUkdHn5187bpWzhGjZJr9EbX7eeclwPk'}/sendMessage`, body)
+					.then(res => console.log(res))
+			}
+
+		} catch(e) {
+			console.log(e);
+		}
+	};
+
 	return (
 		<div className="w-full h-full bg-[#FFC000] ">
 			<section className="max-w-[1040px] w-full mx-auto py-12 px-5 md:px-6 xl:px-0  lg:py-16">
 				<h2 className=" uppercase text-2xl lg:text-5xl font-semibold mb-6 md:mb-16 leading-normal ">
 					Расскажите о задачах, <br /> мы оперативно свяжемся:
 				</h2>
-				<form className="w-full gap-3 md:flex justify-between items-center relative ">
+				<form
+					onSubmit={handleSubmit(onSubmit)}
+					className="w-full gap-3 md:flex justify-between items-center relative "
+				>
 					<div className=" md:w-2/5 relative ">
 						<input
 							type="text"
 							id="name"
+							{...register("userName", {required: true, pattern: /^[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u})}
 							placeholder=" "
 							className={`${aboutCss.input} relative mb-4 md:mb-0 z-10 bg-transparent border-b-2 border-b-[#000] w-full h-14 text-black pt-2 outline-none peer autofill:bg-[#FFC000] `}
 						/>
@@ -22,20 +62,26 @@ export default function Contact() {
 						>
 							Имя
 						</label>
+						{errors?.userName && <span className="text-red-600" >Поле имя обязательное!</span>}
 					</div>
 					<div className=" md:w-2/5 relative ">
 						<input
 							type="tel"
 							id="tel"
+							{...register("phone", {
+								required: true,
+								pattern:
+									/^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/,
+							})}
 							placeholder=" "
-							name="tel"
-							className={` ${aboutCss.input} relative  mb-9 md:mb-0  z-10 bg-transparent border-b-2 border-b-[#000] w-full h-14 text-black pt-2 outline-none peer`}
+							className={` ${aboutCss.input} relative mb-4 md:mb-0 z-10 bg-transparent border-b-2 border-b-[#000] w-full h-14 text-black pt-2 outline-none peer`}
 						/>
-						<label className=" z-10 peer-focus:font-medium absolute text-black  duration-500 transform -translate-y-5 scale-75 top-3 left-0  origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0   text-2xl font-semibold">
+						<label className=" -z-2 peer-focus:font-medium absolute text-black  duration-500 transform -translate-y-5 scale-75 top-3 left-0  origin-[0] peer-focus:left-0 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0  text-2xl font-semibold">
 							Номер телефона
 						</label>
+						{errors?.phone && <span className="text-red-600" >Нам нужен ваш номер, что-бы позже перезвонить!</span>}
 					</div>
-					<button className=" uppercase flex items-center gap-2 py-3  px-8 text-white font-semibold bg-black rounded-full">
+					<button className="mt-9 uppercase flex items-center gap-2 py-3  px-8 text-white font-semibold bg-black rounded-full">
 						отправить{" "}
 						<svg
 							xmlns="http://www.w3.org/2000/svg"

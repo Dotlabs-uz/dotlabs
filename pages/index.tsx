0f12d0b5739d76ff78715a4582c209126f9efada
Mirsaid25 <siddikovmirsaid01@gmail.com>
2023-11-04 14:07:30 +0500

    anumation

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,15 +23,15 @@ export default function Home({ data }: any) {
     const [isOpen, setIsOpen] = useState(false);
     const translation: any = useContext(TranslateContext);
 
-    useEffect(() => {
-        const body: any = document.querySelector("body");
+    // useEffect(() => {
+    //     const body: any = document.querySelector("body");
 
-        if (isOpen) {
-            body.style.overflow = "hidden";
-        } else {
-            body.style.overflow = "scroll";
-        }
-    }, [isOpen]);
+    //     if (isOpen) {
+    //         body.style.overflow = "hidden";
+    //     } else {
+    //         body.style.overflow = "scroll";
+    //     }
+    // }, [isOpen]);
 
     return (
         <Layout>
@@ -56,35 +56,47 @@ export default function Home({ data }: any) {
                             {translation?.section1?.buttonText}
                         </button>
                     </div>
-                    {!isOpen ? (
-                        <div
-                            className="hidden md:flex justify-center items-center w-full h-[256px] rounded-xl cursor-pointer bg-[url(/images/video.png)] bg-no-repeat bg-center bg-cover"
-                            onClick={() => setIsOpen(!isOpen)}
-                        >
-                            <BsFillPlayFill color="white" size="50" />
-                        </div>
-                    ) : (
-                        <>
-                            <div className="w-full"></div>
+                    <div
+                        className="hidden md:flex justify-center items-center w-full h-[256px] rounded-xl cursor-pointer bg-[url(/images/video.png)] bg-no-repeat bg-center bg-cover"
+                        onClick={() => setIsOpen(!isOpen)}
+                    >
+                        <BsFillPlayFill color="white" size="50" />
+                    </div>
+                    
+                    <AnimatePresence>
+                        {isOpen && (
                             <motion.div
                                 animate={{ backgroundColor: "#0000008d" }}
                                 className="fixed top-0 left-0 w-full h-full backdrop-blur-sm"
                             >
                                 <motion.div
-                                    className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 hidden md:flex justify-center items-center w-full rounded-xl cursor-pointer bg-[url(/images/video.png)] bg-no-repeat bg-center bg-cover"
-                                    onClick={() => setIsOpen(!isOpen)}
                                     initial={{
-                                        width: "300px",
-                                        height: "200px",
+                                        width: "500px",
+                                        height: "256px",
                                     }}
-                                    animate={{ width: "80vw", height: "80vh" }}
+                                    animate={{
+                                        width: "80vw",
+                                        height: "80vh",
+                                        top: "50%",
+                                        left: "50%",
+                                    }}
                                     transition={{ type: "tween", duration: 1 }}
+                                    exit={{
+                                        width: "500px",
+                                        height: "256px",
+                                        top: "33%",
+                                        left: "64%",
+                                    }}
+                                    onClick={() => setIsOpen(!isOpen)}
+                                    className={
+                                        "bg-black fixed top-[33%] left-[64%] -translate-x-1/2 -translate-y-1/2 rounded-xl bg-[url(/images/video.png)] bg-no-repeat bg-center bg-cover flex items-center justify-center"
+                                    }
                                 >
                                     <BsFillPlayFill color="white" size="50" />
                                 </motion.div>
                             </motion.div>
-                        </>
-                    )}
+                        )}
+                    </AnimatePresence>
                 </div>
             </section>
 

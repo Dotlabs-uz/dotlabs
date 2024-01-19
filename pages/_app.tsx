a64180b98395bde309bdccf41fc57a4be604fd96
Mirsaid25 <siddikovmirsaid01@gmail.com>
2024-01-19 13:13:51 +0500

    off fpixel pageview

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,19 +21,19 @@ export default function App({ Component, pageProps }: AppProps) {
 
 
 
-    useEffect(() => {
-        // This pageview only triggers the first time (it's important for Pixel to have real information)
-        fbq.pageview();
+    // useEffect(() => {
+    //     // This pageview only triggers the first time (it's important for Pixel to have real information)
+    //     fbq.pageview();
     
-        const handleRouteChange = () => {
-          fbq.pageview();
-        };
+    //     const handleRouteChange = () => {
+    //       fbq.pageview();
+    //     };
     
-        router.events.on("routeChangeComplete", handleRouteChange);
-        return () => {
-          router.events.off("routeChangeComplete", handleRouteChange);
-        };
-      }, [router.events]);
+    //     router.events.on("routeChangeComplete", handleRouteChange);
+    //     return () => {
+    //       router.events.off("routeChangeComplete", handleRouteChange);
+    //     };
+    //   }, [router.events]);
 
 
 

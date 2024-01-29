f2e4f034848e6ac0f082a995eef5fa428eaad4df
Daler <dsarifkulo021@gmail.com>
2024-01-29 19:16:23 +0500

    added yandex metrika

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,38 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+	return (
+		<Html lang="en">
+			<Head />
+			<body>
+				<Main />
+				<NextScript />
+				<script
+					dangerouslySetInnerHTML={{
+						__html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+              m[i].l=1*new Date();
+              for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+              k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(96294714, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+              })`,
+					}}
+				/>
+				<noscript>
+					<div>
+						<img
+							src="https://mc.yandex.ru/watch/96294714"
+							style={{ position: "absolute", left: "-9999px" }}
+							alt=""
+						/>
+					</div>
+				</noscript>
+			</body>
+		</Html>
+	);
 }

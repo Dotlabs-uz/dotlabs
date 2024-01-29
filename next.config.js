d66d353801d3e93cdf9469e265cb10c37ad683e7
Daler <dsarifkulo021@gmail.com>
2024-01-29 19:23:15 +0500

    added remote patterns for yandex

--- next.config.js
+++ next.config.js
@@ -4,14 +4,21 @@ const path = require('path')
 
 
 const nextConfig = {
-  reactStrictMode: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-  i18n: {
-    locales: ["uz", "ru", "en"],
-    defaultLocale: "ru",
- },
+	reactStrictMode: true,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+	i18n: {
+		locales: ["uz", "ru", "en"],
+		defaultLocale: "ru",
+	},
+	images: {
+		remotePatterns: [{
+			protocol: 'https',
+			hostname: "mc.yandex.ru",
+			port: ''
+		}]
+	}
 }
 
 module.exports = nextConfig
